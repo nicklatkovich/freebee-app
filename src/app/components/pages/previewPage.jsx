0fe94fd54817f0606c314746fc250f722dfb0277
George Puisha <stearis@tut.by>
2018-03-04 00:33:34 +0300

    Add article styles

--- src/app/components/pages/previewPage.jsx
+++ src/app/components/pages/previewPage.jsx
@@ -25,7 +25,7 @@ export default class PreviewPage extends Component {
                     strength = { 500 }
           >
             <div className="preview-page__parallax">
-              <div className="preview-page__container-logo">
+              <div className="preview-page__logo_container">
                 <div className="preview-page__title">
                     <h1>Freebee</h1>
                 </div>  
@@ -35,7 +35,19 @@ export default class PreviewPage extends Component {
           </Parallax>
           <Parallax strength = { 200 }
           >   
-            <div style = {{ height: '800px' }} />
+            <div className="preview-page__article_container">
+              <div className="preview-page__article">
+                <div className="preview-page__article_title">
+                  <h2>Lorem ispum</h2>
+                </div>
+                <div className="preview-page__article_description">
+                  <small>dolor sit amet, consectetur adipiscing elit</small>
+                </div>
+                <div className="preview-page__article_content">
+                  <p>Aenean finibus, est vitae sodales auctor, lacus magna auctor neque, id vestibulum ante enim ut quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce condimentum, nisl quis imperdiet tempus, lacus purus eleifend neque, at lobortis leo ex eu felis. Maecenas laoreet rhoncus felis, sed auctor leo. Nam maximus vestibulum nunc, a posuere ipsum accumsan nec. Ut lobortis orci sit amet erat euismod commodo. Duis mattis vestibulum eros id eleifend. Etiam pharetra nibh ex, et eleifend nulla pellentesque quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi interdum volutpat dignissim.</p>
+                </div>  
+              </div>
+            </div>
           </Parallax>
         </div>
       </div>
