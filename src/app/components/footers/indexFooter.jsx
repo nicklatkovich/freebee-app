8757ec7dbf1b96f168e23be1edac2a58b52d428c
Puysha <e.puysha@itransition.corp>
2018-03-05 17:48:34 +0300

    Add links footer section, add logo image

--- src/app/components/footers/indexFooter.jsx
+++ src/app/components/footers/indexFooter.jsx
@@ -9,17 +9,17 @@ export default () => {
           <p>Freebee в социальных сетях:</p>
         </div>
         <div className="index-footer__section-social">
-          <a className="index-footer__section-social-link" href="#">
+          <a className="index-footer__link" href="#">
             <FontAwesome  name='vk' 
                           size='2x'
               />
           </a> 
-          <a className="index-footer__section-social-link" href="#">
+          <a className="index-footer__link" href="#">
             <FontAwesome  name='facebook' 
                           size='2x'
             />
           </a> 
-          <a className="index-footer__section-social-link" href="#">
+          <a className="index-footer__link" href="#">
             <FontAwesome  name='instagram' 
                           size='2x'
             />
@@ -27,7 +27,21 @@ export default () => {
         </div>
       </div>
       <div className="index-footer__section">
-        
+        <div className="index-footer__section-text">
+          <p>Полезные ссылки:</p>
+        </div>
+        <div className="index-footer__section-list">
+          <a className="index-footer__link" href="https://trello.com/b/G4wBsmj9/freebee">
+            <small>Доска Trello</small>
+          </a>
+          <a className="index-footer__link" href="https://github.com/GitStearis/freebee-app">
+            <small>Репозиторий на GitHub</small>
+          </a>
+          <a className="index-footer__link" href="http://telegra.ph/Social-Weekend-Hackathon--kak-ehto-bylo-02-26">
+            <small>Статья на Телеграфе</small>
+          </a>
+        </div>
+          
       </div>
     </div>
   )
--- src/app/components/pages/previewPage.jsx
+++ src/app/components/pages/previewPage.jsx
@@ -4,10 +4,12 @@ import IndexHeader from '../headers/indexHeader'
 import IndexFooter from '../footers/indexFooter'
 import NextBtn from '../buttons/nextBtn'
 
+import Logo from '../../../res/images/freebee-logo.png';
+
 export default class PreviewPage extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state={}
   }
 
   componentDidMount() {
@@ -17,24 +19,24 @@ export default class PreviewPage extends Component {
 
   render() {
     return (
-      <div className = "preview-page">
+      <div className="preview-page">
         <IndexHeader />
-        <div className = "preview-page__content" >
-          <Parallax blur = { 5 }
-                    bgImage = { 'https://pp.userapi.com/c543103/v543103443/193ca/nmsVfIkGtz8.jpg' }
-                    bgImageAlt = "Shopping center"
-                    strength = { 500 }
+        <div className="preview-page__content" >
+          <Parallax blur={ 5 }
+                    bgImage={ 'https://pp.userapi.com/c543103/v543103443/193ca/nmsVfIkGtz8.jpg' }
+                    bgImageAlt="Shopping center"
+                    strength={ 500 }
           >
             <div className="preview-page__parallax">
               <div className="preview-page__logo-container">
                 <div className="preview-page__title">
                     <h1>Freebee</h1>
                 </div>  
-                <img className="preview-page__logo" src = "https://lh5.googleusercontent.com/AHeqaB6TizRrhgqOhwo8pBaX7yOP0gNTev7lfnBTM5bIq_D6OHWcv1rqXgFMMdkr029j0T5sehJzZD8Fkd5t=w1366-h662-rw" alt = "Freebee logo" />   
+                <img className="preview-page__logo" src={ Logo } alt="Freebee logo" />   
               </div>
             </div>
           </Parallax>
-          <Parallax strength = { 200 }
+          <Parallax strength={ 200 }
           >   
             <div className="preview-page__article-container">
               <div className="preview-page__article">
